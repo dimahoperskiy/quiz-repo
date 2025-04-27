f47ea5b471bd07f7849660158b8d03caa168395d
d.khoperskii <d.khoperskii@xsolla.com>
2025-04-27 14:44:37 +0300

    Add Mongo to test 2

--- quiz-service/tests/quiz.test.ts
+++ quiz-service/tests/quiz.test.ts
@@ -1,23 +1,22 @@
-import request from 'supertest';
-import app from '../src/app';
-import mongoose from 'mongoose';
+import request from "supertest";
+import app from "../src/app";
+import mongoose from "mongoose";
 
-describe('Quiz API', () => {
-    beforeAll(async () => {
-        await mongoose.connect('mongodb://localhost:27017/quiz-test');
-    });
+describe("Quiz API", () => {
+  beforeAll(async () => {
+    // await mongoose.connect('mongodb://localhost:27017/quiz-test');
+    await mongoose.connect("mongodb://mongo:27017/quiz-test");
+  });
 
-    afterAll(async () => {
-        await mongoose.connection.dropDatabase();
-        await mongoose.disconnect();
-    });
+  afterAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.disconnect();
+  });
 
-    it('должен отдавать страны для квиза', async () => {
-        const res = await request(app)
-            .post('/api/quiz/start')
-            .send({ count: 5 });
+  it("должен отдавать страны для квиза", async () => {
+    const res = await request(app).post("/api/quiz/start").send({ count: 5 });
 
-        expect(res.statusCode).toBe(200);
-        expect(Array.isArray(res.body)).toBe(true);
-    });
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
 });
