912af4ab2274171fa0bcc07b085a9961bb143f51
d.khoperskii <d.khoperskii@xsolla.com>
2026-03-27 20:04:30 +0300

    deployment fixes

--- world-quiz/vite.config.ts
+++ world-quiz/vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/world-quiz/',
+  base: '/quiz-repo/',
   plugins: [react()],
   resolve: {
     alias: {
